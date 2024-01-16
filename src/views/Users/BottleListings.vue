f1636f9b6dda0f08ebbda8326d50fe946ab0fd43
Shannie Kwok <shanniekwok.2021@scis.smu.edu.sg>
2024-01-16 18:23:42 +0800

    added back arrow to navigate back to main page

--- src/views/Users/BottleListings.vue
+++ src/views/Users/BottleListings.vue
@@ -201,7 +201,13 @@
             <div class="row">
                 <!-- show matching # of search results -->
                 <div class="col-8 d-flex align-items-center">
+                    <!-- back button -->
                     <span style="display: inline-block;">
+                        <span class="pe-2">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-arrow-left-circle" viewBox="0 0 16 16" v-on:click="resetListings">
+                                <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-4.5-.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5z"/>
+                            </svg>
+                        </span>
                         <h5 style="display: inline-block;"> Showing {{ filteredListings.length }} results for "{{ searchTerm }}" </h5> 
                         <h5 style="display: inline-block;"> &nbsp; | &nbsp; </h5>
                         <!-- show options to add listings -->
@@ -545,6 +551,13 @@
                     this.search = false;
                 }
             },
+
+            // for resetting listings (show full listings)
+            resetListings() {
+                this.filteredListings = this.listings;
+                this.search = false;
+                this.searchInput = '';
+            }
         },
     };
 </script>
