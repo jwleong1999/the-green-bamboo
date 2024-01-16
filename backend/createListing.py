bf0160291ac32565bfea6b784acb08c54258798b
DumplingJ10 <jwleong.199@gmail.com>
2024-01-16 17:44:19 +0800

    Create createListing.py
    New function that inserts new bottle listing into MongoDB via POST request. Code will need to be refined once data model for Listing is out.

---
+++ backend/createListing.py
@@ -0,0 +1,70 @@
+import bson
+import json
+from bson import json_util
+from flask import Flask, request, jsonify
+from flask_pymongo import PyMongo
+from flask_cors import CORS
+from werkzeug.local import LocalProxy
+
+from pymongo.errors import DuplicateKeyError, OperationFailure
+from bson.objectid import ObjectId
+from bson.errors import InvalidId
+
+app = Flask(__name__)
+CORS(app)  # Enable CORS for all routes
+app.config["MONGO_URI"] = "mongodb+srv://jwleong2020:uOfXCrxLPCjgyA92@greenbamboo.wbiambw.mongodb.net/GreenBamboo?retryWrites=true&w=majority"
+db = PyMongo(app).db
+
+def parse_json(data):
+    return json.loads(json_util.dumps(data))
+
+#[POST]
+# require bottle name, drink type, producer
+# optional drink category, country of origin, bottler, age, ABV, 88bamboo website review (if applicable), official description
+@app.route("/createListings", methods= ['POST'])
+def createListings():
+    rawBottle = request.get_json()
+    rawBottleName = rawBottle["Expression Name"]
+
+
+    existingBottle = db.Listings.find_one({"Expression Name": rawBottleName})
+
+    # Check whether bottle with the same name exists in the database
+    if(existingBottle != None):
+        return jsonify(
+            {
+                "code": 400,
+                "data": {
+                    "bottleName": rawBottleName
+                },
+                "message": "Bottle already exists."
+            }
+        ), 400
+    
+    # Reference the bottle model
+    # newBottle = Bottle(**rawBottle)
+
+    try:
+        insertResult = db.Listings.insert_one(rawBottle)
+
+        return jsonify( 
+            {   
+                "code": 201,
+                "data": rawBottle
+            }
+        ), 201
+    except Exception:
+        print(Exception)
+        return jsonify(
+            {
+                "code": 500,
+                "data": {
+                    "bottleName": rawBottleName
+                },
+                "message": "An error occurred creating the listing."
+            }
+        ), 500
+    
+
+if __name__ == "__main__":
+    app.run(debug=True)
