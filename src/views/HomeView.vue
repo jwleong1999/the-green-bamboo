e63a81591843969e0a2d37b75ea9cf52b6d6734a
Ryan Lim <ryanlim.2021@scis.smu.edu.sg>
2024-01-21 18:28:02 +0800

    HomeView marked as component playground

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,4 +1,8 @@
+<!-- Default generated view by Vue CLI, should not be actively used in the final version. -->
+<!-- Currently used as Component Testing PLAYGROUND. -->
+
 <template>
+  <NavBar />
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
@@ -9,11 +13,13 @@
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import NavBar from '@/components/NavBar.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
-  }
+    HelloWorld,
+    NavBar
+}
 }
 </script>
