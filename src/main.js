e5e33f69089be39b52eb7aef4207aded11823038
Ryan Lim <ryanlim.2021@scis.smu.edu.sg>
2024-01-21 21:10:30 +0800

    CSS ordering fixed
    CSS ordering is now:
    - Bootstrap
    - App.vue
    - ???
    - global.css

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 // Javascript file that kickstarts application. Imports root component, Bootstrap, and router, and mounts the app to the DOM.
 
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
+import './assets/global.css';
 
 createApp(App).use(router).mount('#app')
