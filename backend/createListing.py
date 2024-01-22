e40e02b3f91af539d783f0d815c1e369950f7dc1
DumplingJ10 <jwleong.199@gmail.com>
2024-01-22 22:19:46 +0800

    Integrated dataclass into createListing.py
    Modified data.py to include asdict()function to be imported

--- backend/createListing.py
+++ backend/createListing.py
@@ -10,6 +10,8 @@ from pymongo.errors import DuplicateKeyError, OperationFailure
 from bson.objectid import ObjectId
 from bson.errors import InvalidId
 
+import data
+
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
 app.config["MONGO_URI"] = "mongodb+srv://jwleong2020:uOfXCrxLPCjgyA92@greenbamboo.wbiambw.mongodb.net/GreenBamboo?retryWrites=true&w=majority"
@@ -24,15 +26,16 @@ def parse_json(data):
 @app.route("/createListings", methods= ['POST'])
 def createListings():
     rawBottle = request.get_json()
-    rawBottleName = rawBottle["Expression Name"]
+    rawBottleName = rawBottle["drinkName"]
 
 
-    existingBottle = db.Listings.find_one({"Expression Name": rawBottleName})
+    # existingBottle = db.Listings.find_one({"Expression Name": rawBottleName})
+    existingBottle = db.Listings.find_one({"drinkName": rawBottleName})
 
     # Check whether bottle with the same name exists in the database
     if(existingBottle != None):
         return jsonify(
-            {
+            {   
                 "code": 400,
                 "data": {
                     "bottleName": rawBottleName
@@ -41,11 +44,11 @@ def createListings():
             }
         ), 400
     
-    # Reference the bottle model
-    # newBottle = Bottle(**rawBottle)
+    
+    newBottle = data.Listings(**rawBottle)
 
     try:
-        insertResult = db.Listings.insert_one(rawBottle)
+        insertResult = db.Listings.insert_one(data.asdict(newBottle))
 
         return jsonify( 
             {   
@@ -53,8 +56,8 @@ def createListings():
                 "data": rawBottleName
             }
         ), 201
-    except Exception:
-        print(Exception)
+    except Exception as e:
+        print(str(e))
         return jsonify(
             {
                 "code": 500,
