1d0cada5d3ef47822e0a32d8099ab659d6b57c22
Shannie Kwok <shanniekwok.2021@scis.smu.edu.sg>
2024-01-27 03:30:58 +0800

    added in navbar based on 88b's feedback (need to edit once login is done)

--- src/views/Producers/BottleListings.vue
+++ src/views/Producers/BottleListings.vue
@@ -10,7 +10,7 @@
                 </div>
                 <!-- search bar -->
                 <div class="col-md-6">
-                    <input class="search-bar form-control rounded fst-italic" type="text" placeholder="What are you drinking today?" style="height: 50px;" v-model="searchInput" v-on:keyup.enter="searchListings"> 
+                    <input class="search-bar form-control rounded fst-italic" type="text" placeholder="What are you drinking today?" style="height: 50px;" v-model="searchInput" v-on:keyup.enter="helperSearch"> 
                 </div>
                 <div>
                     <!-- profile icon -->
@@ -26,11 +26,75 @@
             </div>
         </nav>
     </div>
+
     <!-- collapsible content -->
-    <div class="collapse" id="navbarToggleExternalContent"> <!-- TODO: check what content to put here -->
-        <div class="p-4">
-            <h5 class="text-body-emphasis h4">Collapsed content</h5>
-            <span class="text-body-secondary">Toggleable via the navbar brand.</span>
+    <div class="collapse" id="navbarToggleExternalContent"> <!-- TODO: modify links accordingly -->
+        <div class="row pt-2">
+            <div class="col-7"></div>
+            <!-- [specific] for different accounts -->
+            <div class="col-2 navbar-dropdown text-end">
+                <!-- Users -->
+                <div class="p-4" id="Users">
+                    <h3> Users </h3>
+                    <!-- TODO: link to the user's profile page -->
+                    <a href="" class="default-clickable-text"> 
+                        <h5> View Profile </h5>
+                    </a>
+                    <!-- TODO: link to the user's dashboard -->
+                    <a href="" class="default-clickable-text"> 
+                        <h5> My Drink Stats </h5>
+                    </a>
+                    <!-- TODO: link to lists -->
+                    <a href="" class="default-clickable-text"> 
+                        <h5> My Bookmarked Drinks </h5>
+                    </a>
+                </div>
+
+                <!-- Venues -->
+                <div class="p-4" id="Venues">
+                    <h3> Venues </h3>
+                    <!-- TODO: link to the venue's profile page -->
+                    <a href="" class="default-clickable-text"> 
+                        <h5> View Profile </h5>
+                    </a>
+                    <!-- TODO: link to the venues's dashboard -->
+                    <a href="" class="default-clickable-text"> 
+                        <h5> My Dashboard </h5>
+                    </a>
+                    <!-- TODO: link to edit menu page -->
+                    <a href="" class="default-clickable-text"> 
+                        <h5> My Menu </h5>
+                    </a>
+                </div>
+
+                <!-- Producers -->
+                <div class="p-4" id="Producers">
+                    <h3> Producers </h3>
+                    <!-- TODO: link to the producers's profile page -->
+                    <a href="" class="default-clickable-text"> 
+                        <h5> View Profile </h5>
+                    </a>
+                    <!-- TODO: link to the producer's dashboard -->
+                    <a href="" class="default-clickable-text"> 
+                        <h5> My Dashboard </h5>
+                    </a>
+                    <!-- TODO: link to new listings page -->
+                    <a href="" class="default-clickable-text"> 
+                        <h5> Add a New Listing </h5>
+                    </a>
+                </div>
+
+                <!-- [common] for all accounts -->
+                <a href="www.88bamboo.co" class="primary-clickable-text">
+                    <h4> 
+                        Read & Learn
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
+                            <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8"/>
+                        </svg>
+                    </h4>
+                </a>
+            </div>
+            <div class="col-3"></div>
         </div>
     </div>
 
