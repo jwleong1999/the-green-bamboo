0375880a7d94226e900bc1bcd009c0794f395246
liyingpoh <liying.poh.2021@scis.smu.edu.sg>
2024-02-08 15:36:24 +0800

    changed display based on database

--- src/views/UserProfile.vue
+++ src/views/UserProfile.vue
@@ -26,7 +26,7 @@
                     <!-- additional information -->
                     <div class="mt-3">
                         <span class="float-start"><b>Member since</b></span>
-                        <!-- todo --> <span class="float-end">Sept 2021</span>
+                        <!-- todo --> <span class="float-end">{{ joinDate }}</span>
                         <br/>
                         <span class="float-start"><b>Drink of Choice</b></span>
                         <span class="float-end">{{ drinkChoice }}</span>
@@ -227,10 +227,10 @@
                         <h4 class="mt-4">Recent Reviews</h4>
                         <div>
                             <div v-for="(review, index) in reviews" :key="index">
-                                <div style="display: flex" class="mb-3" v-if="review.userID?.$oid == user._id?.$oid">
+                                <div style="display: flex" class="mb-3" v-if="review.userID?.$oid === displayUserID && review.reviewType === 'Listing'">
                                     <img :src=" 'data:image/png;base64,' + (review.photo||defaultDrinkImage)" alt="" class="border border-dark-subtle border-2 bottle-img me-3">
                                     <div>
-                                        <h3>{{ review.reviewTarget }}</h3>
+                                        <h3>{{ getListingName(review.reviewTarget) }}</h3>
                                         <h2>
                                             {{ review.rating }}
                                             <svg class="mb-2" xmlns="http://www.w3.org/2000/svg" height="18" width="20.25" viewBox="0 0 576 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M287.9 0c9.2 0 17.6 5.2 21.6 13.5l68.6 141.3 153.2 22.6c9 1.3 16.5 7.6 19.3 16.3s.5 18.1-5.9 24.5L433.6 328.4l26.2 155.6c1.5 9-2.2 18.1-9.7 23.5s-17.3 6-25.3 1.7l-137-73.2L151 509.1c-8.1 4.3-17.9 3.7-25.3-1.7s-11.2-14.5-9.7-23.5l26.2-155.6L31.1 218.2c-6.5-6.4-8.7-15.9-5.9-24.5s10.3-14.9 19.3-16.3l153.2-22.6L266.3 13.5C270.4 5.2 278.7 0 287.9 0zm0 79L235.4 187.2c-3.5 7.1-10.2 12.1-18.1 13.3L99 217.9 184.9 303c5.5 5.5 8.1 13.3 6.8 21L171.4 443.7l105.2-56.2c7.1-3.8 15.6-3.8 22.6 0l105.2 56.2L384.2 324.1c-1.3-7.7 1.2-15.5 6.8-21l85.9-85.1L358.6 200.5c-7.8-1.2-14.6-6.1-18.1-13.3L287.9 79z"/></svg>
@@ -422,12 +422,13 @@ export default {
             user: {},
 
             // user being viewed
-            // displayUserID: "65b327d5687b64f8302d56ef",
-            displayUserID: "65b327d5687b64f8302d56ee",
+            displayUserID: "65b327d5687b64f8302d56ef",
+            // displayUserID: "65b327d5687b64f8302d56ee",
             displayUser: {},
 
             drinkChoice: "",
             drinkCount: 0,
+            joinDate: "",
 
             userBookmarks: {},
             // data from database
@@ -499,6 +500,12 @@ export default {
 
                 this.drinkChoice = this.getDrinkOfChoice();
                 this.drinkCount = this.getDrinkCount();
+
+                const dateString = this.user.joinDate.$date;
+                const dateParts = dateString.split('-');
+                const year = dateParts[0];
+                const month = new Date(dateString).toLocaleString('default', { month: 'long' });
+                this.joinDate = `${month} ${year}`;
 
                 this.userDrinkChoice = this.getDrinkOfChoice(this.user);
                 this.displayUserDrinkChoice = this.getDrinkOfChoice(this.displayUser);
@@ -724,6 +731,15 @@ export default {
         searchResult() {
             this.drinkSearchResults = this.listings.filter(listing => listing.listingName.toLowerCase().includes(this.drinkSearch.toLowerCase())).map(listing => listing.listingName);
         },
+        async checkReviewType(listingID) {
+            const review = this.reviews.find(review => review.listingID.$oid === listingID.$oid);
+            return review.reviewType == "Listing"
+        },
+        getListingName(listingID) {
+            if (this.listings) {
+                return this.listings.find(listing => listing._id.$oid === listingID.$oid).listingName;
+            }
+        }
         
     },
 };
