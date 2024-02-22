a9a99bea5e8070b02144b804360533b227fedb29
DumplingJ10 <jwleong.199@gmail.com>
2024-02-22 18:03:27 +0800

    change port

--- backend/createAccount.py
+++ backend/createAccount.py
@@ -72,4 +72,4 @@ def createAccount():
 
 # -----------------------------------------------------------------------------------------
 if __name__ == "__main__":
-    app.run(debug=True, port = 5202)
+    app.run(debug=True, port = 5400)
