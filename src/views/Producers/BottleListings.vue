62447033cea6b928926b4526b44dc7b265558e4f
mffoo13 <fooyaoshengmatthew@gmail.com>
2024-03-29 02:44:36 +0800

    autocomplete with linking to review done
    TBD if need to do the linking to the account

--- src/views/Producers/BottleListings.vue
+++ src/views/Producers/BottleListings.vue
@@ -681,10 +681,18 @@
                                         </b>
                                     </router-link>
                                     &nbsp;rated {{ review['rating'] }}
+                                    
                                     <!-- star icon -->
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-fill me-3" viewBox="0 0 16 16">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-fill me-1" viewBox="0 0 16 16">
                                         <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
                                     </svg>
+                                    
+                                    <b>
+                                        <a v-if="review.location !== ''" :href="'https://www.google.com/maps/search/' + review.location" class="me-3" style="color: inherit"> 
+                                            at {{ review.location }}
+                                        </a>
+                                    </b>
+                                    
                                     <span v-if="checkModFromUserID(review.userID)" class="badge rounded-pill" style="color: black; background-color: white;">Moderator</span>
                                     <!-- Insert Edit modal here -->
 
@@ -1862,7 +1870,7 @@
                     "aroma" : this.aroma,
                     "taste" : this.taste,
                     "finish" : this.finish,
-                    "location" :this.selectedLocationId,
+                    "location" :this.selectedLocation,
                     "willRecommend": this.wouldRecommend,
                     "taggedUsers": this.friendTagList,
                     "wouldBuyAgain": this.wouldBuyAgain,
@@ -1917,7 +1925,7 @@
                     "aroma" : this.aroma,
                     "taste" : this.taste,
                     "finish" : this.finish,
-                    "location" :this.selectedLocationId,
+                    "location" :this.selectedLocation,
                     "taggedUsers": this.friendTagList,
                     "willRecommend": this.wouldRecommend,
                     "wouldBuyAgain": this.wouldBuyAgain,
